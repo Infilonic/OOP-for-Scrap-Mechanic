64a2fac19895d9127873590eb4f22472cbd0bc75
Sheg <infilonic@users.noreply.github.com>
2019-08-08 18:46:44 +0200

    Fix issues with typeOf function

--- ReservedKeywords/TypeOfKeyword.lua
+++ ReservedKeywords/TypeOfKeyword.lua
@@ -4,7 +4,7 @@ function typeOf(object)
     if concreteType ~= "table" then
         concreteType = type(object)
     elseif object.getType ~= nil and type(object.getType) == "function" then
-        concreteType = object.getType()
+        concreteType = object:getType() or concreteType -- Pass self even if it is being ignored, to maintain compatibility with instances of the type Scriptable
     end
 
     return concreteType
