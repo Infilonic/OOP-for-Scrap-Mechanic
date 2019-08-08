d5a44afb00040e8a3d9a6cf8a76faf52903ea7c1
Sheg <infilonic@users.noreply.github.com>
2019-08-08 03:49:36 +0200

    Update typeOf keyword function

--- ReservedKeywords/TypeOfKeyword.lua
+++ ReservedKeywords/TypeOfKeyword.lua
@@ -4,7 +4,7 @@ function typeOf(object)
     if concreteType ~= "table" then
         concreteType = type(object)
     elseif object.getType ~= nil and type(object.getType) == "function" then
-        concreteType = object:getType()
+        concreteType = object.getType()
     end
 
     return concreteType
